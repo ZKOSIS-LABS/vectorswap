122c1ba3439d602a7ae91459a030a69fb688f90f
crypto-lord123 <sahibbharajj@gmail.com>
2024-12-23 10:04:24 +0300

    tuf

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import SwapComponent from '../components/SwapComponent'
 export default function Home() {
   return (
     <div className='w-full h-screen flex flex-col items-center justify-center bg-[#0c0f24]'>
+      <div className='mainz' ></div>
       <Header />
       <SwapComponent />
       {/* <Footer /> */}
