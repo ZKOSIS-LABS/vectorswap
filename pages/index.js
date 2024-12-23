bed924c7d5916b1b9289f82d6b4d1950326f6b41
crypto-lord123 <sahibbharajj@gmail.com>
2024-12-23 10:12:22 +0300

    utcjh

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import SwapComponent from '../components/SwapComponent'
 
+
 export default function Home() {
   return (
     <>
